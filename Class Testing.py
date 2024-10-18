83474caacae0be298dfa4e83e69a4fe6f037da41
30083609 <30083609@student.toiohomai.ac.nz>
2024-10-18 22:36:04 +1300

    Add files via upload
    Updated code, also making sure all files are uploaded.

---
+++ Class Testing.py
@@ -0,0 +1,22 @@
+class human:
+    """
+    Class representing human.
+
+    Args:
+        name (str): Description of name.
+        age (str): Description of age.
+        sex (str): Description of sex.
+        location (str): Description of location.
+    """
+
+    def __init__(self, name, age, sex, location):
+        self.name = name
+        self.age = age
+        self.sex = sex
+        self.location = location
+
+human_instance = human('<add text>', '<add text>', '<add text>', '<add text>')
+print(f"Name: {human_instance.<add text>}")
+print(f"Age: {human_instance.<add text>}")
+print(f"Sex: {human_instance.<add text>}")
+print(f"Location: {human_instance.<add text>}")
---
+++ Function Testing.py
@@ -0,0 +1,13 @@
+def human(name='default_value', age='default_value', sex='default_value', location='default_value'):
+    """
+    Function to human.
+
+    Args:
+        name (str): Description of name.
+        age (str): Description of age.
+        sex (str): Description of sex.
+        location (str): Description of location.
+    """
+
+    print(f"Hello, {name}! You are {age} years old.")
+    # TODO: Add your logic here
